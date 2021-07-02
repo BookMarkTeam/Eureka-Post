2ae84eb1615597c3dda8c5599bd62b1b65158d9c
ambosing <ambosing_@naver.com>
2021-07-02 10:55:55 +0900

    Eureka-Post add

---
+++ src/main/java/com/example/eurekapost/EurekaPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurekapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaPostApplication.class, args);
+    }
+
+}
