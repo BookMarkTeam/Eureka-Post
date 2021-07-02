2fe97fe5e319ffd3e065e79429cb6c8033c5cfbd
ambosing <ambosing_@naver.com>
2021-07-02 11:25:12 +0900

    Eureka-Post add

--- src/main/java/com/example/eurekapost/EurekaPostController.java
+++ src/main/java/com/example/eurekapost/EurekaPostController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Post/")
+@RequestMapping("/posts/")
 @Slf4j
 public class EurekaPostController {
     @GetMapping("/test")
